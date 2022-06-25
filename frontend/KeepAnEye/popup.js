a723163f6611144aae98ada1d9da4a935202eec8
gianni <gianni.crivello96@gmail.com>
2022-06-25 08:41:25 -0700

    frontend

--- frontend/KeepAnEye/popup.js
+++ frontend/KeepAnEye/popup.js
@@ -1,23 +1,33 @@
-const btnLogin = document.getElementById("btnLogin");
-        const btnRegister = document.getElementById("btnRegister");
         const number = document.getElementById("number");
-        
-        btnLogin.addEventListener("click", () => {
-            fetch("https://hackinghiestauth-uo5lual43q-uc.a.run.app/login", {
+        const confirmation = document.getElementById("confirmation")
+        const btnLogin = document.getElementById("btnLogin");
+	const btnRegister = document.getElementById("btnRegister");
+        const query = document.getElementById("query");
+
+        btnLogin.addEventListener("click", async () => {
+		let url = `http://localhost:8000/api/user/auth?phone=%2B${number.value}&OTP=${confirmation.value}`
+		console.log(url);
+            const result = await fetch(url, {
 		    "method": "POST",
 		    "mode": "no-cors",
 		    "headers": {"content-type": "application/json"},
-		    "body": JSON.stringify({
-			    "username": "shibby",
-			    "number": "12345"
-		    })})
-		.then(res => console.log(res.json))
+		    "body":{
+		    }})
+            const jResult = await result.json();
+                console.log(jResult);
+		chrome.storage.sync.set({accessToken: jResult.token}, function() {
+			console.log('accessToken is set to ' + jResult.token);
+		});
+		window.location.href="query.html";
         })
  
         btnRegister.addEventListener("click", async () => {
-		var data = {"phone": "%2B" + number};
-		var url = new URL("http://127.0.0.1:8000/api/user/otp?")
-		for (let k in data) {url.searchParams.append(k, data[k])}
+		console.log(number)
+
+
+		let url = `http://localhost:8000/api/user/otp?phone=%2B${number.value}` 
+
+		console.log(url);
 
 		const result = await fetch(url, {
                     "method": "POST",
@@ -27,5 +37,27 @@ const btnLogin = document.getElementById("btnLogin");
             
                     }}) 
             const jResult = await result.json();
+		window.location.href="confirmation.html"
                 console.log(jResult);
         })
+
+
+        query.addEventListener("click", async () => {
+		let url = "http://localhost:8000/api/user/check?content=fuck%20you"
+		const token = await chrome.storage.sync.get(['accessToken'])
+		console.log("grabbed the token off the DB: ", token.accessToken);
+		const bearer = `Bearer ${token.accessToken}`
+		console.log("built bearer token: ", bearer);
+		const result = await fetch(url, {
+		    "method": "POST",
+		    "mode": "no-cors",
+		    headers: {
+			    'Authorization': bearer, 
+			    'accept': "application/json"
+		    }, 
+		    body: {
+		    }})
+		const jResult = await result.json();
+		console.log(jResult)
+	})
+
